ccc2c9b969ff249595f6e45b0ceccc245f71ddb2
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-12 17:45:42 +0800

    first commit

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [articles, setArticles] = useState([])
   useEffect(() => {
-    fetch("http://localhost:4000/articles")
-    .then((res) => res.json())
+    fetch('http://localhost:4000/articles')
+    .then((res) => {return res.json()})
     .then((data) => {setArticles(data)});}, []);
   return (
     <main>
