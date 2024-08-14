11303d02860d47aa79f458c1c58348f9dcbe4e2a
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-14 17:59:49 +0800

    frontend editing

--- app/page.js
+++ app/page.js
@@ -3,14 +3,41 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [articles, setArticles] = useState([])
-  useEffect(() => {
-    fetch('http://localhost:4000/articles')
+
+  function addMe () {
+    const name = prompt("Enter the name")
+    fetch(`http://localhost:4000/categories?name=${name}`)
     .then((res) => {return res.json()})
-    .then((data) => {setArticles(data)});}, []);
+    .then (() => {reload()})
+  };
+   function reload () {
+    fetch('http://localhost:4000/categories/list')
+    .then((res) => {return res.json()})
+    .then((data) => {setArticles(data)})
+   }
+
+   function edit () {
+
+   }
+
+   function trash () {
+      articles.filter (()=>{articles[0]})
+   }
+
+  useEffect(() => {
+    reload()
+   ;}, []);
+
   return (
     <main>
       {articles.map((article) => (
-        <div key={article.id}>{article.title}</div>))}
+        <div>
+          <div key={article.id}>{article.name}</div>
+          <button onClick={edit} key={article.id}>{article.edit}</button>
+          <button onClick={trash} key={article.id}>{article.delete}</button>
+        </div>
+        ))}
+        <button onClick={addMe}>Add</button>
     </main>
   )
   ;
