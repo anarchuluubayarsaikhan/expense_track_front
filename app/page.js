7fa91fe0dfb1925ba982de6f8f489afd9b331feb
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-19 18:42:12 +0800

    delete added

--- app/page.js
+++ app/page.js
@@ -1,43 +1,54 @@
 "use client"
+
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [articles, setArticles] = useState([])
 
-  function addMe () {
-    const name = prompt("Enter the name")
-    fetch(`http://localhost:4000/categories?name=${name}`)
-    .then((res) => {return res.json()})
-    .then (() => {reload()})
-  };
-   function reload () {
+  function loadList () {
     fetch('http://localhost:4000/categories/list')
     .then((res) => {return res.json()})
-    .then((data) => {setArticles(data)})
-   }
+    .then((data) => {setArticles(data)});
+  }
+  useEffect ( () => {
+    loadList ()
+  }, []) 
+  
+  function Add () {
+    const name = prompt("Name")
+      fetch(`http://localhost:4000/categories?name=${name}`)
+      .then(() => loadList())
+  }
+  function deleteMe(id) {
+    fetch(`http://localhost:4000/categories/${id}`, {
+      method: "DELETE",
+    })
+    .then (() => {loadList()})
 
-   function edit () {
+  }
+  function editMe(id) {
+    const name = prompt ("Name..")
+    fetch(`http://localhost:4000/categories/${id}`, {
+      method:"PUT",
+      body: name,
+    }
+    )
+    .then (() => {loadList()})
+  }
 
-   }
+  
+  
 
-   function trash () {
-      articles.filter (()=>{articles[0]})
-   }
-
-  useEffect(() => {
-    reload()
-   ;}, []);
-
+  
   return (
     <main>
-      {articles.map((article) => (
-        <div>
-          <div key={article.id}>{article.name}</div>
-          <button onClick={edit} key={article.id}>{article.edit}</button>
-          <button onClick={trash} key={article.id}>{article.delete}</button>
-        </div>
-        ))}
-        <button onClick={addMe}>Add</button>
+         {articles.map((article,index) => (
+        <div key={article.name} className="flex gap-4">
+          {article.name}
+          <button className="border border-gray-500 rounded-md p-2 bg-slate-300" onClick={() => {editMe(article.id)}}>Edit</button>
+          <button className="border border-gray-500 rounded-md p-2 bg-red-200" onClick={() => {deleteMe(article.id)}}>Delete</button>
+        </div>))}
+        <button onClick={Add}>Add me</button>
     </main>
   )
   ;
